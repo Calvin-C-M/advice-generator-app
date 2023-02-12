fbb267b6b5c468ba33b81618cea76317ff423bc1
Calvin-C-M <montolalu28@gmail.com>
2023-02-12 17:45:11 +0700

    Imported custom styling from guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      md: '375px',
+      lg: '1440px'
+    },
+    extend: {
+      colors: {
+        primary: {
+          1: 'hsl(193, 38%, 86%)',
+          2: 'hsl(150, 100%, 66%)'
+        },
+        neutral: {
+          1: 'hsl(217, 19%, 38%)',
+          2: 'hsl(217, 19%, 24%)',
+          3: 'hsl(218, 23%, 16%)',
+        },
+      },
+      fontSize: {
+        quote: '28px'
+      },
+      fontWeight: {
+        bold: 800
+      }
+    },
   },
   plugins: [],
 }
